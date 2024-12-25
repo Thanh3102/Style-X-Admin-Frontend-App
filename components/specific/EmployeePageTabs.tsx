5145979d0006bbaddc72fd403ef380deee9dbf82
Thanh3102 <thanhduong200602@gmail.com>
2024-12-25 18:05:46 +0700

    Fix eslint error

--- components/specific/EmployeePageTabs.tsx
+++ components/specific/EmployeePageTabs.tsx
@@ -14,7 +14,7 @@ type Props = {
 };
 
 const EmployeePageTabs = ({ employeeData, permissions }: Props) => {
-  let tabs = [
+  const tabs = [
     {
       id: "employees",
       label: "Nhân viên",
